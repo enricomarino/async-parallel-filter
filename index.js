7aaf78cb8ea568315f4c7c9f14a902475d593c17
Enrico Marino <enrico.marino@email.com>
2013-01-09 19:45:37 +0100

    implemented

--- index.js
+++ index.js
@@ -7,3 +7,48 @@
  * @license MIT
  */
 
+/*
+ * Expose `parallel_filter`
+ */
+
+module.exports = parallel_filter;
+
+/*
+ * parallel-filter
+ * Filter each item of 'array' by 'iterator' in parallel
+ * and call 'callback' when done
+ *
+ * @param {Array} array array
+ * @param {Function} iterator iterator
+ * @param {Function} callback callback
+ * @api public
+ */
+
+function parallel_filter(array, iterator, callback) {
+  var results = [];
+  var completed = 0;
+  var len = array.length;
+  var i;
+
+  function complete(err, result) {
+    if (err) {
+      callback(err);
+      callback = function () {};
+      return;
+    }
+    results[index] = result ? [value] : [];
+    completed += 1;
+    if (completed === len) {
+      callback(concat.apply([], results));
+      return;
+    }
+  }
+
+  function iterate(index, value) {
+    iterator(value, complete);
+  }
+
+  for (i = 0; i < len; i += 1) {
+    iterate(i, array[i]);
+  }
+};
